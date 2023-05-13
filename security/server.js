6c64918bfce5e624cd0463494577de1ff2a3d0bb
伊東和成 <itokazunari5375@icloud.com>
2023-05-13 22:08:56 +0900

    feat: first-commit

---
+++ security/server.js
@@ -0,0 +1,19 @@
+// Expressライブラリを読み込む
+const express = require("express");
+
+// Expressアプリケーションを生成する
+const app = express();
+
+// サーバーがリッスンするポートを設定する
+const port = 3000;
+
+// ルートパス('/')にGETリクエストが来た時のハンドラを設定する
+// このハンドラは "Top Page" という文字列をレスポンスとして返す
+app.get("/", (req, res, next) => {
+  res.end("Top Page");
+});
+
+// 指定したポートでサーバーを起動し、起動成功をコンソールに表示する
+app.listen(port, () => {
+  console.log("サーバー起動");
+});
